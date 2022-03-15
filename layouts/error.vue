8c90d944d2a52400a5983639c0e373d2d058ad27
Lissandre <lissandre.pasdeloup@gmail.com>
2022-03-15 15:14:47 +0100

    Layouts default & error

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div >
+      <h1 class="text-9xl font-black text-gray-300">{{ error.statusCode }}</h1>
+      <span class="text-right block text-gray-500">{{ error.statusCode === 404 ? $t('error.notfound') : $t('error.else') }}</span>
+    </div>
+    <!-- <NuxtLink to="/">Home page</NuxtLink> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error'
+  }
+</script>
