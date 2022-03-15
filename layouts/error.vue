9bffa8ae617fd59951ed87237406a1127e340f0d
Lissandre <lissandre.pasdeloup@gmail.com>
2022-03-15 16:15:18 +0100

    Back to home button on error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,7 +4,7 @@
       <h1 class="text-9xl font-black text-gray-300">{{ error.statusCode }}</h1>
       <span class="text-right block text-gray-500">{{ error.statusCode === 404 ? $t('error.notfound') : $t('error.else') }}</span>
     </div>
-    <!-- <NuxtLink to="/">Home page</NuxtLink> -->
+    <NuxtLink :to="localePath('index')" class="absolute bottom-0 pb-4 text-gray-500">{{ $t('error.backlink') }}</NuxtLink>
   </div>
 </template>
 
