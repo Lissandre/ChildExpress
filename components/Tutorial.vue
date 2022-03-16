c4a6c36dcf65f8f69c70e14943ec1a94fd83eff2
Lissandre <lissandre.pasdeloup@gmail.com>
2022-03-16 17:40:51 +0100

    Link to 404 for test

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,8 +1,9 @@
 <template>
   <div
-    class="flex items-center justify-center min-h-screen"
+    class="flex items-center justify-center min-h-screen flex-col"
   >
     {{ $t('home.title') }}
+    <NuxtLink :to="localePath('/asdf')" class="block uppercase">> go to 404 <</NuxtLink>
   </div>
 </template>
 
