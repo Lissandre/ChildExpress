dc98a235a0307d21e2bfd48fd4e800a5dcf2e61c
Lissandre <lissandre.pasdeloup@gmail.com>
2022-04-05 11:19:13 +0200

    Locales > Update nested object creation

--- locales/fr/index.js
+++ locales/fr/index.js
@@ -2,7 +2,16 @@ const files = {}
 
 require.context('./content', true, /\.json$/).keys().forEach(url => {
   const file = require('./content/' + url.substring(2))
-  files[url.substring(2).replace('.json','')] = file
+  createNestedObject(files, url.substring(2).replace('.json', '').split('/'), file)
 })
+
+function createNestedObject(base, names, value) {
+  let lastName = arguments.length === 3 ? names.pop() : false
+  for (let i = 0; i < names.length; i++) {
+    base = base[names[i]] = base[names[i]] || {}
+  }
+  if (lastName) base = base[lastName] = value
+  return base
+}
 
 export default files
