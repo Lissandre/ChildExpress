c65cc5bd54be4bc36533685a0fe705c7404050d2
Lissandre <lissandre.pasdeloup@gmail.com>
2022-04-07 14:13:27 +0200

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,8 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   generate: {
     fallback: true,
   },
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'ChildExpress',
     htmlAttrs: {
@@ -19,43 +16,24 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~plugins/scene.client.js', mode: 'client' }],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: false,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  components: true,
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/composition-api/module',
     '@pinia/nuxt',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/i18n',
-  ],
-
+  modules: ['@nuxtjs/axios', '@nuxtjs/i18n'],
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-
   tailwindcss: {
     exposeConfig: false,
     viewer: false,
   },
-
-  // i18n module configuration
   i18n: {
     locales: [
       {
@@ -85,7 +63,7 @@ export default {
         prettify: false,
       },
     },
-    transpile: ['three'],
+    // transpile: ['three'],
     extend(config) {
       config.module.rules.push({
         test: /\.(glsl|vs|fs|frag|vert)$/,
