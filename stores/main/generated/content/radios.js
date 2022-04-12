3f8f26d591f3a801577bd392923a3ab2ab06d6c6
Lissandre <lissandre.pasdeloup@gmail.com>
2022-04-12 12:06:11 +0200

    fix form 3

--- stores/main/generated/content/radios.js
+++ stores/main/generated/content/radios.js
@@ -1,3 +1,3 @@
 export default [
-
+  { id: 'handsSize', value: 0.5 }
 ]
--- stores/main/generated/content/ranges.js
+++ stores/main/generated/content/ranges.js
@@ -1,8 +1,6 @@
 export default [
   { id: 'noseSize', value: 0.5 },
   { id: 'earsSize', value: 0.5 },
-  { id: 'skinColor', value: #e66465 },
   { id: 'handsSize', value: 0.5 },
-  { id: 'overallSize', value: 0.5 },
-  { id: 'handsSize', value: 0.5 }
+  { id: 'overallSize', value: 0.5 }
 ]
