57e92dd547c3bdda52aa9723718a5bfe574f534f
Alexis Sejourné <alexis.sejourne@edu.gobelins.fr>
2022-05-25 15:06:04 +0200

    feat(buttoncool): yes

--- components/Focus.vue
+++ components/Focus.vue
@@ -2,18 +2,14 @@
   <div class="relative bottom-2 p-4 z-50 pointer-events-auto">
     <button
       @click="changeFocus"
-      class="right-10 top-10 pb-4 text-gray-500 pointer-events-auto absolute"
+      class="right-10 top-10 pb-4 text-gray-500 pointer-events-auto absolute button-face"
       v-if="currentFocus"
-    >
-    face
-    </button>
+    ></button>
     <button
       @click="changeFocus"
-      class="right-20 top-10 pb-4 text-gray-500 pointer-events-auto absolute"
+      class="right-20 top-10 pb-4 text-gray-500 pointer-events-auto absolute button-pipette"
       v-if="!currentFocus"
-    >
-    pipette
-    </button>
+    ></button>
   </div>
 </template>
 
@@ -47,7 +43,6 @@ export default {
 
 <style scoped>
 button {
-  background: blue;
   height: 60px;
   width: 60px;
   box-shadow: 1px 5px 5px rgb(15 84 228 / 56%);
@@ -55,5 +50,22 @@ button {
   display: flex;
   justify-content: center;
   align-items: center;
+  transition: all ease-out 0.2s;
+  transform: scale3d(0.8, 0.8, 0.8);
+  perspective-origin: 500% 200%;
 }
-</style> 
+button:hover {
+  transform: scale3d(1, 1, 1)
+}
+.button-face {
+  background: no-repeat center center url('@/assets/images/face.svg');
+}
+.button-pipette {
+  background: no-repeat center center url('@/assets/images/pipette.svg');
+}
+
+.button-face, .button-pipette {
+  background-color: #0F54E4;
+  background-size: 40px 40px;
+}
+</style>
