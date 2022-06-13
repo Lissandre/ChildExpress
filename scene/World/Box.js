b924afbb976fd2d75d2484c736bd98c60094207d
Ahlecss <alex.sejourne1@gmail.com>
2022-06-13 17:10:57 +0200

    feat(box):

--- scene/World/Box.js
+++ scene/World/Box.js
@@ -1,5 +1,6 @@
 import { Mesh, MeshBasicMaterial, Object3D, BoxGeometry, RepeatWrapping, CanvasTexture, LinearFilter, ClampToEdgeWrapping, DoubleSide, ImageLoader } from 'three'
 
+
 export default class Box {
   constructor(options) {
     // Options
@@ -10,6 +11,8 @@ export default class Box {
     this.container = new Object3D()
     this.container.name = 'Box'
 
+    this.oldMeasure = { width: -30 }
+
     // this.createBox()
     // this.setMovement()
 
@@ -17,61 +20,61 @@ export default class Box {
   createBox(job, name, xtras, personality, iq, weight, height) {
 
     setTimeout(() => {
-    const canvas = this.makeCanvas(32, job, name, xtras, personality, iq, weight, height);
+      const canvas = this.makeCanvas(32, job, name, xtras, personality, iq, weight, height);
 
-    this.texture = new CanvasTexture(canvas);
-    this.texture.needsUpdate = true;
-    this.texture.minFilter = LinearFilter;
-    this.texture.wrapS = ClampToEdgeWrapping;
-    this.texture.wrapT = ClampToEdgeWrapping;
-    
-    
-    this.texture.wrapS = RepeatWrapping;
-    this.texture.wrapT = RepeatWrapping;
-   
-    
-    const material = new MeshBasicMaterial({
-      map: this.texture,
-      // map: this.assets.textures.map_box,
-      side: DoubleSide,
-      transparent: true,
-    });
+      this.texture = new CanvasTexture(canvas);
+      this.texture.needsUpdate = true;
+      this.texture.minFilter = LinearFilter;
+      this.texture.wrapS = ClampToEdgeWrapping;
+      this.texture.wrapT = ClampToEdgeWrapping;
 
-    console.log(this.assets.models)
-    this.box = this.assets.models.baby_box.scene
-    console.log(this.box)
-    //this.babyMin = this.baby.geometry.boundingBox.min.z
-    //this.babyMax = this.baby.geometry.boundingBox.max.z
 
-    //this.babyMin = this.baby.children[2].geometry.boundingBox.min.z
-    //this.babyMax = this.baby.children[2].geometry.boundingBox.max.z
-    // this.baby.children[2].material.transparent = true
+      this.texture.wrapS = RepeatWrapping;
+      this.texture.wrapT = RepeatWrapping;
 
-    // faire des variables
 
-    this.box.traverse(function (child) {
+      const material = new MeshBasicMaterial({
+        map: this.texture,
+        // map: this.assets.textures.map_box,
+        side: DoubleSide,
+        transparent: true,
+      });
 
-      if (child.isMesh) {
+      console.log(this.assets.models)
+      this.box = this.assets.models.baby_box.scene
+      console.log(this.box)
+      //this.babyMin = this.baby.geometry.boundingBox.min.z
+      //this.babyMax = this.baby.geometry.boundingBox.max.z
 
-        console.log(material)
+      //this.babyMin = this.baby.children[2].geometry.boundingBox.min.z
+      //this.babyMax = this.baby.children[2].geometry.boundingBox.max.z
+      // this.baby.children[2].material.transparent = true
 
-        console.log(child.geometry.attributes.uv);
-        child.material = material; // assign your diffuse texture here
+      // faire des variables
 
-      }
+      this.box.traverse(function (child) {
 
-    });
-    // this.map1 = this.assets.textures.map_box
-    // this.map1.wrapS = RepeatWrapping;
-    // this.map1.wrapT = RepeatWrapping;
+        if (child.isMesh) {
 
-    this.container.add(this.box)
-    this.box.scale.set(-3, -3, -3)
-    this.box.rotation.set(Math.PI, 0, 0)
+          console.log(material)
 
-    this.box.position.set(1, 1, 3)
+          console.log(child.geometry.attributes.uv);
+          child.material = material; // assign your diffuse texture here
 
-  }, 2000)
+        }
+
+      });
+      // this.map1 = this.assets.textures.map_box
+      // this.map1.wrapS = RepeatWrapping;
+      // this.map1.wrapT = RepeatWrapping;
+
+      this.container.add(this.box)
+      this.box.scale.set(-3, -3, -3)
+      this.box.rotation.set(Math.PI, 0, 0)
+
+      this.box.position.set(1, 0, 0)
+
+    }, 2000)
 
   }
 
@@ -81,64 +84,121 @@ export default class Box {
     console.log(this.assets.textures.map_box)
     this.ctx.canvas.width = 2048
     this.ctx.canvas.height = 2048
-    
-    this.ctx.drawImage(this.assets.textures.map_box.image, 0, 0, 2048, 2048);
+
+    var imageObj = new Image();
+    console.log(this.assets)
+    imageObj.src = this.assets.textures.map_box.image.src;
+    imageObj.addEventListener("load", () => {
+      console.log('her')
+      console.log(this.ctx)
+      this.ctx.drawImage(this.assets.textures.map_box.image, 0, 0, 2048, 2048);
+      console.log('draw')
+      this.ctx.font = "40pt Calibri";
+      this.ctx.fillText("My TEXT!", 20, 20);
 
 
-    const font = `${size}px bold NeueBit`;
-    this.ctx.font = font;
-    // measure how long the name will be
-    const doubleBorderSize = borderSize * 2;
+      const font = `${size}px bold NeueBit`;
+      this.ctx.font = font;
+      // measure how long the name will be
+      const doubleBorderSize = borderSize * 2;
 
-    // need to set font again after resizing canvas
-    this.ctx.font = font;
-    this.ctx.textBaseline = 'top';
+      // need to set font again after resizing canvas
+      this.ctx.font = font;
+      this.ctx.textBaseline = 'top';
 
 
-    this.ctx.fillStyle = 'blue';
-    //ctx.fillRect(0, 0, width, height);
-    this.ctx.fillStyle = 'red';
+      this.ctx.fillStyle = 'blue';
+      //ctx.fillRect(0, 0, width, height);
+      this.ctx.fillStyle = 'black';
 
-    this.ctx.fillRect(1500, 1500,  1000, 1500);
+      //this.ctx.fillRect(0, 0, 1000, 1500);
 
-    
-    /* Job */
-    this.ctx.font =`100px bold NeueBit`;
-    this.ctx.fillText(name, 50, 0);
-    
-    /* Nom */
-    this.ctx.fillStyle = 'white';
-    this.ctx.font =`80px bold NeueBit`;
-    this.ctx.fillText(job, 50, 50);
-    
-    /* xtras */
-    this.ctx.font =`40px bold NeueBit`;
-    this.ctx.strokeStyle = "white";
-    this.ctx.fillText(xtras[0], 50, 100);
-    this.ctx.strokeRect(5, 100, 250, 50);
 
-    /* personnalité */
-    
-    console.log(personality)
-    var height = 100
-    personality.forEach((perso) => {
-      this.ctx.fillText(perso.label, 50, height);
-      height += 50
+      /* Job */
 
-      this.ctx.strokeStyle = "white";
-      this.ctx.fillStyle = "white";
+      this.ctx.save()
+      this.ctx.font = `100px bold NeueBit`;
+      this.ctx.translate(550, 1200);
+      this.ctx.rotate(-0.5 * Math.PI);
+      this.ctx.fillText(name, 0, 0);
+      this.ctx.restore()
 
-      this.ctx.strokeRect(50, height, 500, 50);
-      this.ctx.fillRect(50, height,  perso.percentage * 3, 50);
-      height += 50
-    })
+      /* Nom */
+      this.ctx.save()
+      this.ctx.translate(650, 1200);
+      this.ctx.rotate(-0.5 * Math.PI);
+      this.ctx.font = `80px bold NeueBit`;
+      this.ctx.fillText(job, 0, 0);
+      this.ctx.restore()
+
+      /* xtras */
+
+      var xtrasFinal = Array.from(xtras)
+      for (const [key, value] of Object.entries(xtrasFinal)) {
+        this.ctx.save()
+        this.ctx.translate(650, 1200);
+        this.ctx.rotate(-0.5 * Math.PI);
+        this.ctx.font = `20px bold NeueBit`;
+        this.ctx.strokeStyle = "black";
+        const uppercase = value.toUpperCase()
+        this.ctx.fillText(uppercase, this.oldMeasure.width + 20, 100);
+        const measure = this.ctx.measureText(uppercase)
+
+
+        this.ctx.strokeRect(this.oldMeasure.width + 10, 100, (measure.width + 20), 20);
+        this.oldMeasure = measure
+        console.log(this.oldMeasure)
+        this.ctx.restore()
+      }
+
+      /* personnalité */
+
+      console.log(personality)
+      var height = 80
+      personality.forEach((perso) => {
+        this.ctx.save()
+        this.ctx.translate(2000, 800);
+        this.ctx.rotate(0.5 * Math.PI);
+
+        this.ctx.strokeStyle = "black";
+        this.ctx.fillStyle = "black";
+        this.ctx.font = `22px bold NeueBit`;
+
+
+        this.ctx.strokeRect(50, height, 300, 30);
+        this.ctx.fillRect(50, height, perso.percentage * 3, 30);
+        this.ctx.font = `28px bold Calibri`;
+        this.ctx.fillText(perso.percentage + '%', 370, height);
+
+        height += 40
+
+        this.ctx.fillText(perso.label, 50, height);
+        height += 40
+
+        this.ctx.restore();
+      })
+
+
 
 
     /* qi */
-    // ctx.fillText(iq, 50, 200);
+    console.log(iq)
+    this.ctx.save()
+    this.ctx.translate(1800, 800);
+    this.ctx.rotate(0.5 * Math.PI);
+    this.ctx.font = `42px bold Calibri`;
+    this.ctx.fillText(iq, 40, 260);
+    this.ctx.restore()
 
     /* poids */
-    // ctx.fillText(weight + 'KG', 50, 250);
+
+    
+    this.ctx.save()
+    this.ctx.translate(800, 500);
+    this.ctx.rotate(-1);
+    this.ctx.font = `42px bold Calibri`;
+    this.ctx.fillText(weight + 'KG', 400, 400);
+    this.ctx.restore()
 
     /* taille */
     // ctx.fillText(height, 50, 300);
@@ -151,47 +211,13 @@ export default class Box {
     /* taille */
     // ctx.fillText(height, 50, 300);
 
+  })
     return this.ctx.canvas;
-  }
-  imageCanvas() {
-    const loader = new ImageLoader();
-    const ctx = document.createElement('canvas').getContext('2d');
-
-    loader.load(
-      // resource URL
-      '@assets/textures/map_box.png',
-
-      // onLoad callback
-      function (image) {
-        // use the image, e.g. draw part of it on a canvas
-        this.img = image
-      },
-    )
-
-    ctx.drawImage(this.img, 0, 0)
-
-
-    /*var img1 = new Image();
-    
-        img1.crossOrigin='anonymous'
-        img1.onload = () => {
-          // onLoad callback
-          console.log(typeof img1)
-          // use the image, e.g. draw part of it on a canvas
-        };
-        img1.src = this.assets.textures.map_box
-    
-        console.log(typeof this.assets.textures.map_box)
-        //ctx.drawImage(this.assets.textures.map_box, 0, 0)
-    */
-  }
-  writeOnBox(job) {
-    console.log('writed')
   }
   setMovement() {
     this.time.on('tick', () => {
       this.texture.needsUpdate = true;
-      
+
       // this.box.rotation.y += 0.001 * this.time.delta
     })
   }
--- stores/main/state.js
+++ stores/main/state.js
@@ -19,7 +19,7 @@ export const state = () => {
 
     //   // << END CONSTANTS >>
     // },
-    activeForm: 5,
+    activeForm: 4,
     totalForms: 5,
     health: true,
     face: 'body',
